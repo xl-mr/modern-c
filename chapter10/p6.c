b24ad18a131984043033b779787b09c1890d7460
xulin <631813620@qq.com>
2019-05-21 22:36:29 +0800

    add

--- chapter10/p6.c
+++ chapter10/p6.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define STACK_SIZE 100
 
@@ -45,10 +46,24 @@ int pop(void)
         return e;
     }
 }
+
+void read_expr(void) {
+    char ch;
+    printf("Enter an RPN expression:");
+    
+    do {
+        scanf(" %c", &ch);       
+        
+        if (isalnum(ch)) {
+            push(ch);
+        } else if (ch == '*' || ch == '/' | ch == '+' || ch == '-') {
+                   
+        }
+    } while (ch != 'q');   
+}
     
 int main(void)
 {
-    char c;
 
    return 0;
 }
