55113ea0fe1c1f9916c06ddce42025e860f95033
xulin <xulin@chelun.com>
2019-06-17 20:25:07 +0800

    add

--- chapter17/link.c
+++ chapter17/link.c
@@ -38,7 +38,31 @@ struct node *read_numbers(void)
 
 struct node *search_list(struct node *list, int n)
 {
-    for () {}
+    for (; list != NULL && list->value != n; list = list->next) {
+        ;
+    }
+    return list;
+}
+
+struct node *delete_from_list(struct node *list, int n)
+{
+    struct node *cur, *prev;
+
+    for (cur = list, prev = NULL; cur != NULL && cur->value == n; prev = cur, cur = cur->next) {
+        ;
+    }
+
+    if (cur == NULL) {
+        return list;
+    }
+    if (prev == NULL) {
+        list = list->next;
+    } else {
+        prev->next = cur->next;
+    }
+    free(cur);
+
+    return list;
 }
 
 int main(void)
