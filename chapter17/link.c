cbef0b41f0e741a209720396a50f87b852fd71a6
xulin <xulin@chelun.com>
2019-06-17 20:28:32 +0800

    change

--- chapter17/link.c
+++ chapter17/link.c
@@ -6,6 +6,21 @@ struct node {
     struct node *next;
 };
 
+void add_to_list2(struct node **list, int n) 
+{
+    struct node *new_node;
+
+    new_node = (struct node *) malloc(sizeof(struct node));
+    if (new_node == NULL) {
+        printf("Error: malloc failed in add to list\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    new_node->value = n;
+    new_node->next = *list;
+    *list = new_node;
+}
+
 struct node *add_to_list(struct node *list, int n)
 {
     struct node *new_node;
